09e5bc50088426a249db4d31ef7915515ee15be9
Acer_Mortem <Acer_Mortem@yahoo.com>
2016-05-05 23:40:17 -0400

    Added PlayerData

--- src/net/Soultide/PlayerData/DataManager.java
+++ src/net/Soultide/PlayerData/DataManager.java
@@ -1,5 +1,26 @@
 package net.Soultide.PlayerData;
 
+import java.util.HashMap;
+import java.util.UUID;
+
 public class DataManager {
 
+	private static HashMap<UUID, PlayerData> DataList = new HashMap<UUID, PlayerData>();
+	
+	public void loadPlayerData(UUID id){
+		//Load from SQL database
+	}
+	
+	public void savePlayerData(UUID id){
+		//Save to SQL database
+	}
+	
+	public PlayerData getPlayerData(UUID id){
+		if(DataList.containsKey(id)) return DataList.get(id);
+		
+		//In the event of first login or no playerdata
+		PlayerData newData = new PlayerData(id);
+		DataList.put(id, newData);
+		return newData;
+	}
 }
--- src/net/Soultide/PlayerData/PlayerData.java
+++ src/net/Soultide/PlayerData/PlayerData.java
@@ -1,5 +1,37 @@
 package net.Soultide.PlayerData;
 
+import java.util.UUID;
+
 public class PlayerData {
 
+	private UUID id;
+	private int level;
+	private int EXP;
+	private int totalMana;
+	
+	public PlayerData(UUID id){
+		this.id = id;
+		this.level = 1;
+		this.EXP = 0;
+	}
+	
+	public UUID getID(){
+		return this.id;
+	}
+	
+	public int getLevel(){
+		return this.level;
+	}
+	
+	public void incrementLevel(){
+		level++;
+	}
+	
+	public int getEXP(){
+		return this.EXP;
+	}
+	
+	public void addEXP(int add){
+		this.EXP = this.EXP + add;
+	}
 }
