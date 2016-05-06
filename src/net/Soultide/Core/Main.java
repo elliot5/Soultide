71e41eb28c8959b64b71944595f74ada5d592baf
Acer_Mortem <Acer_Mortem@yahoo.com>
2016-05-05 23:40:24 -0400

    Updated Main for all changes

--- src/net/Soultide/Core/Main.java
+++ src/net/Soultide/Core/Main.java
@@ -1,7 +1,40 @@
 package net.Soultide.Core;
 
+import net.Solitude.GUIs.GUIListener;
+
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin{
-
+	
+	public void onEnable(){
+		getLogger().info("Enabling Soultide");
+		registerClasses();
+		registerListeners();
+		registerCommandExecutors();
+	}
+	
+	public void onDisable(){
+		getLogger().info("Disabling Soultide");
+		for(Player p : Bukkit.getOnlinePlayers()){
+			p.kickPlayer("Server Restarting");
+		}
+	}
+	
+	private void registerClasses(){
+		
+	}
+	
+	private void registerListeners(){
+		PluginManager pm = Bukkit.getPluginManager();
+		GUIListener guil = new GUIListener(this);
+		pm.registerEvents(guil, this);
+	}
+	
+	private void registerCommandExecutors(){
+		
+	}
+	
 }
